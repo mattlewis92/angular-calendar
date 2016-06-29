5b6188ddcbc44e62a070c624bbc7d98eee852ab9
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-06-29 11:30:12 +0100

    feat(weekView): add initial week view implementation

--- src/calendarWeekView.component.ts
+++ src/calendarWeekView.component.ts
@@ -1,10 +1,12 @@
 import {Component, Input} from '@angular/core';
-import * as moment from 'moment';
-import {Moment} from 'moment';
 import {NgFor} from '@angular/common';
-import {CalendarDay, CalendarEvent} from './interfaces';
-
-const DAYS_IN_WEEK: number = 7;
+import {
+  WeekDay,
+  CalendarEvent,
+  WeekViewEventRow,
+  getWeekViewHeader,
+  getWeekView
+} from 'calendar-utils';
 
 @Component({
   selector: 'mwl-calendar-week-view',
@@ -16,14 +18,30 @@ const DAYS_IN_WEEK: number = 7;
           <small>{{ day.date.format('D MMM') }}</small>
         </div>
       </div>
+      <div *ngFor="let eventRow of eventRows">
+        <div
+          class="event-container"
+          *ngFor="let event of eventRow.row trackBy event"
+          [style.width]="((100 / 7) * event.span) + '%'"
+          [style.marginLeft]="((100 / 7) * event.offset) + '%'">
+          <div
+            class="event"
+            [class.border-left-rounded]="!event.extendsLeft"
+            [class.border-right-rounded]="!event.extendsRight"
+            [style.backgroundColor]="event.event.color">
+            <a href="javascript:;">{{ event.event.title }}</a>
+          </div>
+        </div>
+      </div>
     </div>
   `,
   styles: [`
-    .week-view {
-      border: 1px solid #e1e1e1;
-    }
     .day-headers {
       display: flex;
+      margin-bottom: 3px;
+      border: 1px solid #e1e1e1;
+      margin-left: 2px;
+      margin-right: 2px;
     }
     .day-headers .header {
       flex: 1;
@@ -36,6 +54,24 @@ const DAYS_IN_WEEK: number = 7;
     .day-headers .header:hover {
       background-color: #ededed;
     }
+    .event-container {
+      display: inline-block;
+    }
+    .event {
+      padding: 3px 10px;
+      font-size: 12px;
+      margin-bottom: 3px;
+      margin-left: 2px;
+      margin-right: 2px;
+    }
+    .border-left-rounded {
+      border-top-left-radius: 5px;
+      border-bottom-left-radius: 5px;
+    }
+    .border-right-rounded {
+      border-top-right-radius: 5px;
+      border-bottom-right-radius: 5px;
+    }
   `],
   directives: [NgFor]
 })
@@ -44,18 +80,24 @@ export class CalendarWeekView {
   @Input() date: Date;
   @Input() events: CalendarEvent[] = [];
 
-  private days: CalendarDay[];
+  private days: WeekDay[];
+  private eventRows: WeekViewEventRow[] = [];
 
   ngOnChanges(changes: any): void {
 
     if (changes.date) {
-      const start: Moment = moment(this.date).startOf('week');
-      this.days = [];
-      for (let i: number = 0; i < DAYS_IN_WEEK; i++) {
-        this.days.push({
-          date: start.clone().add(i, 'days')
-        });
-      }
+      this.days = getWeekViewHeader({
+        viewDate: this.date
+      });
+    }
+
+    if (changes.events || changes.date) {
+
+      this.eventRows = getWeekView({
+        events: this.events,
+        viewDate: this.date
+      });
+
     }
 
   }
