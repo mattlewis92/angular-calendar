f18621c8157eb074353bae4e2d189089bceb3655
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-07-03 17:11:45 +0100

    feat(monthView): match styling of original calendar

--- src/calendarMonthView.component.ts
+++ src/calendarMonthView.component.ts
@@ -6,23 +6,55 @@ import {
 import {NgFor, SlicePipe} from '@angular/common';
 import * as moment from 'moment';
 import {Moment} from 'moment';
-import {CalendarEvent, WeekDay} from 'calendar-utils';
+import {CalendarEvent, WeekDay, getWeekViewHeader} from 'calendar-utils';
+
+interface MonthDay extends WeekDay {
+  inMonth: boolean;
+}
 
 @Component({
   selector: 'mwl-calendar-month-view',
   template: `
     <div class="month-view">
-      <div class="cell-row" *ngFor="let rowIndex of monthOffsets trackBy rowIndex">
-        <div class="cell" *ngFor="let day of days | slice : rowIndex : rowIndex + 7 trackBy day">
-          <div class="cell-top">
-            <span class="day-number">{{ day.date.format('D') }}</span>
+      <div class="cell-row header">
+        <div class="cell" *ngFor="let header of columnHeaders">
+          {{ header.date.format('dddd') }}
+        </div>
+      </div>
+      <div class="days">
+        <div class="cell-row" *ngFor="let rowIndex of monthOffsets">
+          <div
+            class="cell day-cell"
+            *ngFor="let day of days | slice : rowIndex : rowIndex + 7"
+            [class.past]="day.isPast"
+            [class.today]="day.isToday"
+            [class.future]="day.isFuture"
+            [class.weekend]="day.isWeekend"
+            [class.in-month]="day.inMonth"
+            [class.out-month]="!day.inMonth">
+            <div class="cell-top">
+              <span class="day-number">{{ day.date.format('D') }}</span>
+            </div>
           </div>
         </div>
       </div>
     </div>
   `,
   styles: [`
-    .month-view {
+    .header {
+      text-align: center;
+      font-weight: bolder;
+    }
+    .header:hover {
+      background-color: #fafafa;
+    }
+    .header .cell {
+      padding: 5px 0;
+    }
+    .header .cell:hover {
+      background-color: #ededed;
+    }
+    .days {
       border: 1px solid #e1e1e1;
     }
     .cell-row {
@@ -31,15 +63,17 @@ import {CalendarEvent, WeekDay} from 'calendar-utils';
     .cell {
       float: left;
       flex: 1;
-      min-height: 100px;
       display: flex;
       flex-direction: column;
       align-items: stretch;
     }
-    .cell:not(:last-child) {
+    .day-cell {
+      min-height: 100px;
+    }
+    .day-cell:not(:last-child) {
       border-right: 1px solid #e1e1e1;
     }
-    .cell-row:not(:last-child) {
+    .days .cell-row:not(:last-child) {
       border-bottom: 1px solid #e1e1e1;
     }
     .day-number {
@@ -65,6 +99,19 @@ import {CalendarEvent, WeekDay} from 'calendar-utils';
       display: inline-block;
       margin: 2px;
     }
+    .day-cell.out-month .day-number {
+      opacity: 0.1;
+      cursor: default;
+    }
+    .day-cell.weekend .day-number {
+      color: darkred;
+    }
+    .day-cell.today {
+      background-color: #e8fde7;
+    }
+    .day-cell.today .day-number {
+      font-size: 1.9em;
+    }
   `],
   directives: [NgFor],
   pipes: [SlicePipe]
@@ -74,18 +121,31 @@ export class CalendarMonthView implements OnChanges {
   @Input() date: Date;
   @Input() events: CalendarEvent[] = [];
 
-  private days: WeekDay[];
+  private days: MonthDay[];
   private monthOffsets: number[];
+  private columnHeaders: WeekDay[];
 
   ngOnChanges(changes: any): void {
 
     if (changes.date) {
+
+      this.columnHeaders = getWeekViewHeader({
+        viewDate: this.date
+      });
+
       const start: Moment = moment(this.date).startOf('month').startOf('week');
       const end: Moment = moment(this.date).endOf('month').endOf('week');
       this.days = [];
       for (let i: number = 0; i < end.diff(start, 'days') + 1; i++) {
+        const date = start.clone().add(i, 'days');
+        const today = moment().startOf('day');
         this.days.push({
-          date: start.clone().add(i, 'days')
+          date,
+          isPast: date.isBefore(today),
+          isToday: date.isSame(today),
+          isFuture: date.isAfter(today.clone().endOf('day')),
+          isWeekend: [0, 6].indexOf(date.day()) > -1,
+          inMonth: date.clone().startOf('month').isSame(moment(this.date).startOf('month'))
         });
       }
 
