bc07d7577cf8d482c87e5d0bef0a7bec73a8093f
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-08-05 15:54:54 +0100

    refactor(calendarTitle): remove the calendar title pipe
    Closes #40

--- angular2-calendar.ts
+++ angular2-calendar.ts
@@ -2,7 +2,6 @@ import './scss/angular2-calendar.scss';
 import {CalendarMonthView} from './src/components/calendarMonthView.component';
 import {CalendarWeekView} from './src/components/calendarWeekView.component';
 import {CalendarDayView} from './src/components/calendarDayView.component';
-import {CalendarTitle} from './src/pipes/calendarTitle.pipe';
 import {CalendarDate} from './src/pipes/calendarDate.pipe';
 import {CalendarConfig} from './src/providers/calendarConfig.provider';
 import {CalendarDateFormatter} from './src/providers/calendarDateFormatter.provider';
@@ -12,7 +11,6 @@ import {CalendarMomentDateFormatter} from './src/providers/calendarMomentDateFor
 export * from './src/components/calendarMonthView.component';
 export * from './src/components/calendarWeekView.component';
 export * from './src/components/calendarDayView.component';
-export * from './src/pipes/calendarTitle.pipe';
 export * from './src/pipes/calendarDate.pipe';
 export * from './src/providers/calendarConfig.provider';
 export * from './src/providers/calendarMomentDateFormatter.provider';
@@ -24,6 +22,6 @@ export {CalendarEvent, EventAction as CalendarEventAction} from 'calendar-utils'
 // for angular-cli
 export default {
   directives: [CalendarMonthView, CalendarWeekView, CalendarDayView],
-  pipes: [CalendarTitle, CalendarDate],
+  pipes: [CalendarDate],
   providers: [CalendarConfig, CalendarDateFormatter, CalendarNativeDateFormatter, CalendarMomentDateFormatter]
 };
