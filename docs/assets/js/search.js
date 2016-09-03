bf0269cbffa30805eb11a3c551dbaa10b6a8fecc
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-09-03 15:54:01 +0100

    chore: build demo and docs

---
+++ docs/assets/js/search.js
@@ -0,0 +1,1 @@
+﻿var typedoc = typedoc || {};typedoc.search = typedoc.search || {};typedoc.search.data = {"kinds":{"128":"Class"},"rows":[{"id":0,"kind":128,"name":"CalendarModule","url":"classes/calendarmodule.html","classes":"tsd-kind-class"}]};
