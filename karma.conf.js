3340bbbe4c44721ec92ad7617f0154d51fa14ca0
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-09-03 14:12:40 +0100

    chore(tests): fix sourcemaps

--- karma.conf.js
+++ karma.conf.js
@@ -12,7 +12,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'source-map-support'],
+    frameworks: ['mocha'],
 
     // list of files / patterns to load in the browser
     files: [
@@ -30,7 +30,6 @@ module.exports = function(config) {
     },
 
     webpack: {
-      devtool: 'inline-source-map',
       resolve: {
         extensions: ['', '.ts', '.js'],
         alias: {
@@ -58,14 +57,19 @@ module.exports = function(config) {
         emitErrors: !WATCH,
         failOnHint: false
       },
-      plugins: WATCH ? [] : [
+      plugins: (WATCH ? [] : [
         new webpack.NoErrorsPlugin(),
         new StyleLintPlugin({
           syntax: 'scss',
           context: 'scss',
           failOnError: true
         })
-      ]
+      ]).concat([
+        new webpack.SourceMapDevToolPlugin({
+          filename: null,
+          test: /\.(ts|js)($|\?)/i
+        })
+      ])
     },
 
     remapIstanbulReporter: {
