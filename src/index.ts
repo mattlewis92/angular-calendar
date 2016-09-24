f701f8665a85f3dbfe510264ca3e285017cd64b2
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-09-24 19:11:15 +0100

    fix(aot): export all components for aot

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,18 @@ export * from './providers/calendarDateFormatter.provider';
 export * from './interfaces/calendarDateFormatter.interface';
 export * from './calendar.module';
 export {CalendarEvent, EventAction as CalendarEventAction} from 'calendar-utils';
+
+// Private components, don't import or use these within your app, they are only exported so that AOT can work
 export {CalendarDayView} from './components/day/calendarDayView.component';
 export {CalendarWeekView} from './components/week/calendarWeekView.component';
 export {CalendarMonthView} from './components/month/calendarMonthView.component';
+export {CalendarEventActions} from './components/common/calendarEventActions.component';
+export {CalendarEventTitle} from './components/common/calendarEventTitle.component';
+export {CalendarMonthCell} from './components/month/calendarMonthCell.component';
+export {CalendarOpenDayEvents} from './components/month/calendarOpenDayEvents.component';
+export {CalendarWeekViewHeader} from './components/week/calendarWeekViewHeader.component';
+export {CalendarWeekViewEvent} from './components/week/calendarWeekViewEvent.component';
+export {CalendarAllDayEvent} from './components/day/calendarAllDayEvent.component';
+export {CalendarDayViewHourSegment} from './components/day/calendarDayViewHourSegment.component';
+export {CalendarDayViewEvent} from './components/day/calendarDayViewEvent.component';
 export {CalendarTooltipWindow} from './directives/calendarTooltip.directive';
