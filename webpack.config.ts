1b3e2056294882101977f756dbb9ce617ae2bc8b
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-10-24 17:05:26 +0100

    chore: upgrade dev dependencies

--- webpack.config.ts
+++ webpack.config.ts
@@ -22,7 +22,7 @@ module.exports = {
       exclude: /node_modules/
     }, {
       test: /\.scss$/,
-      loader: 'style-loader!css-loader!postcss-loader!sass-loader'
+      loader: 'style-loader!css-loader!sass-loader'
     }, {
       test: /\.css$/,
       loader: 'style-loader!css-loader'
