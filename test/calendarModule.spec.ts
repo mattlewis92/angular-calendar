e273657399a21e30c3997155fd4415426921acc9
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-12-18 14:01:09 +0000

    feat(CalendarModule): allow title and date formatters to be overridden with forRoot
    Closes #90

--- test/calendarModule.spec.ts
+++ test/calendarModule.spec.ts
@@ -1,22 +1,53 @@
-import {
-  inject,
-  TestBed
-} from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { expect } from 'chai';
 import {
   CalendarModule,
-  CalendarDateFormatter
+  CalendarDateFormatter,
+  CalendarEventTitle
 } from './../src';
 
 describe('calendar module', () => {
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({imports: [CalendarModule.forRoot()]});
+  it('should not require providers to be specified when using CalendarModule.forRoot()', () => {
+    TestBed.configureTestingModule({
+      imports: [
+        CalendarModule.forRoot()
+      ]
+    });
+    const dateFormatter: CalendarDateFormatter = TestBed.get(CalendarDateFormatter);
+    expect(dateFormatter instanceof CalendarDateFormatter).to.be.true;
   });
 
-  it('should not require providers to be specified when using CalendarModule.forRoot()',
-    inject([CalendarDateFormatter], (dateFormatter) => {
-    expect(dateFormatter instanceof CalendarDateFormatter).to.be.true;
-  }));
+  it('should allow a custom date formatter to be used', () => {
+    class MyDateFormatter extends CalendarDateFormatter {}
+    TestBed.configureTestingModule({
+      imports: [
+        CalendarModule.forRoot({
+          dateFormatter: {
+            provide: CalendarDateFormatter,
+            useClass: MyDateFormatter
+          }
+        })
+      ]
+    });
+    const dateFormatter: MyDateFormatter = TestBed.get(CalendarDateFormatter);
+    expect(dateFormatter instanceof MyDateFormatter).to.be.true;
+  });
+
+  it('should allow a custom title formatter to be used', () => {
+    class MyEventTitle extends CalendarEventTitle {}
+    TestBed.configureTestingModule({
+      imports: [
+        CalendarModule.forRoot({
+          dateFormatter: {
+            provide: CalendarEventTitle,
+            useClass: MyEventTitle
+          }
+        })
+      ]
+    });
+    const eventTitle: MyEventTitle = TestBed.get(CalendarEventTitle);
+    expect(eventTitle instanceof MyEventTitle).to.be.true;
+  });
 
 });
