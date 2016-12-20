35d2eda15dc37a0792be2be8605bdf9f5b059ccb
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-12-20 09:00:49 +0000

    chore: remove karma coverage

--- karma.conf.ts
+++ karma.conf.ts
@@ -96,7 +96,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage', 'karma-remap-istanbul'],
+    reporters: ['progress', 'karma-remap-istanbul'],
 
     // web server port
     port: 9876,
