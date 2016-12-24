a66b2acfc69750d6bda61511fafe222147b9001d
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-12-24 13:39:21 +0000

    docs: upgrade angular-cli example

--- examples/angular-cli/src/app/app.module.ts
+++ examples/angular-cli/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { CalendarModule } from 'angular-calendar';
 
 import { AppComponent } from './app.component';
@@ -10,8 +12,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
     CalendarModule.forRoot()
   ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
