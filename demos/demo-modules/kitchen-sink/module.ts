0afb27b5a9b8da6450cf64f58851be8a175fa30b
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-12-29 15:55:12 +0000

    refactor: put shared logic back into the kitchen sink demo

--- demos/demo-modules/kitchen-sink/module.ts
+++ demos/demo-modules/kitchen-sink/module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalendarModule } from 'angular-calendar';
-import { DemoUtilsModule } from '../demo-utils/module';
 import { DemoComponent } from './component';
 
 @NgModule({
   imports: [
     CommonModule,
-    DemoUtilsModule,
     CalendarModule.forRoot()
   ],
   declarations: [
