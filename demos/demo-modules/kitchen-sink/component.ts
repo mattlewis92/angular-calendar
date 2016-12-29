ab7ba1cf853e39309d3302d5117047c056d4b01b
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-12-29 15:50:42 +0000

    Revert "refactor: move colors into utils"
    This reverts commit 5acdb9f3307af2e1e985bf6a4c7cf1289329b49e.

--- demos/demo-modules/kitchen-sink/component.ts
+++ demos/demo-modules/kitchen-sink/component.ts
@@ -14,7 +14,21 @@ import {
   CalendarEventAction,
   CalendarEventTimesChangedEvent
 } from 'angular-calendar';
-import { colors } from '../demo-utils/colors';
+
+const colors: any = {
+  red: {
+    primary: '#ad2121',
+    secondary: '#FAE3E3'
+  },
+  blue: {
+    primary: '#1e90ff',
+    secondary: '#D1E8FF'
+  },
+  yellow: {
+    primary: '#e3bc08',
+    secondary: '#FDF1BA'
+  }
+};
 
 @Component({
   selector: 'mwl-demo-component',
