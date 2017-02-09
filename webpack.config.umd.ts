eb5f00f6e167be8663a769aca9ccb3f8501b3f67
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2017-02-09 17:22:12 +0000

    refactor: move external functions out

--- webpack.config.umd.ts
+++ webpack.config.umd.ts
@@ -5,6 +5,51 @@ const postCssFlexibility = require('postcss-flexibility');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 const camelCase = require('lodash.camelcase');
 
+function angularExternals(context, request, callback) {
+
+  if (request.startsWith('@angular/')) {
+    return callback(null, {
+      root: ['ng', camelCase(request.replace(/^\@angular\//, ''))],
+      commonjs: request,
+      commonjs2: request,
+      amd: request
+    });
+  }
+
+  callback();
+
+}
+
+function rxjsExternals(context, request, callback) {
+
+  if (request.startsWith('rxjs/')) {
+    return callback(null, {
+      root: ['rx', request.replace(/^rxjs\//, '')],
+      commonjs: request,
+      commonjs2: request,
+      amd: request
+    });
+  }
+
+  callback();
+
+}
+
+function dateFnsExternals(context, request, callback) {
+
+  if (request.startsWith('date-fns/')) {
+    return callback(null, {
+      root: ['dateFns', camelCase(request.replace(/^date\-fns\//, ''))],
+      commonjs: `${request}/index`,
+      commonjs2: `${request}/index`,
+      amd: `${request}/index`
+    });
+  }
+
+  callback();
+
+}
+
 export default {
   entry: __dirname + '/src/index.umd.ts',
   output: {
@@ -13,38 +58,10 @@ export default {
     libraryTarget: 'umd',
     library: 'angularCalendar'
   },
-  externals: [(context, request, callback) => {
-
-    if (request.startsWith('@angular/')) {
-      return callback(null, {
-        root: ['ng', camelCase(request.replace(/^\@angular\//, ''))],
-        commonjs: request,
-        commonjs2: request,
-        amd: request
-      });
-    }
-
-    if (request.startsWith('rxjs/')) {
-      return callback(null, {
-        root: ['rx', request.replace(/^rxjs\//, '')],
-        commonjs: request,
-        commonjs2: request,
-        amd: request
-      });
-    }
-
-    if (request.startsWith('date-fns/')) {
-      return callback(null, {
-        root: ['dateFns', camelCase(request.replace(/^date\-fns\//, ''))],
-        commonjs: `${request}/index`,
-        commonjs2: `${request}/index`,
-        amd: `${request}/index`
-      });
-    }
-
-    callback();
-
-  }, {
+  externals: [
+    angularExternals,
+    rxjsExternals,
+    dateFnsExternals, {
     'calendar-utils': {
       root: ['calendarUtils'],
       commonjs: 'calendar-utils',
