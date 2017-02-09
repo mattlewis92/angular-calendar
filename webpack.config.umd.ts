4bf6300c7aa3325c244bc64882cebf7a2117d000
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2017-02-09 16:27:00 +0000

    chore: make externals automatic

--- webpack.config.umd.ts
+++ webpack.config.umd.ts
@@ -3,6 +3,7 @@ import * as ExtractTextPlugin from 'extract-text-webpack-plugin';
 const autoprefixer = require('autoprefixer');
 const postCssFlexibility = require('postcss-flexibility');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
+const camelCase = require('lodash.camelcase');
 
 export default {
   entry: __dirname + '/src/index.umd.ts',
@@ -12,114 +13,57 @@ export default {
     libraryTarget: 'umd',
     library: 'angularCalendar'
   },
-  externals: {
-    '@angular/core': {
-      root: ['ng', 'core'],
-      commonjs: '@angular/core',
-      commonjs2: '@angular/core',
-      amd: '@angular/core'
-    },
-    '@angular/common': {
-      root: ['ng', 'common'],
-      commonjs: '@angular/common',
-      commonjs2: '@angular/common',
-      amd: '@angular/common'
-    },
-    '@angular/platform-browser': {
-      root: ['ng', 'platformBrowser'],
-      commonjs: '@angular/platform-browser',
-      commonjs2: '@angular/platform-browser',
-      amd: '@angular/platform-browser'
-    },
-    'rxjs/Subject': {
-      root: ['rx', 'Subject'],
-      commonjs: 'rxjs/Subject',
-      commonjs2: 'rxjs/Subject',
-      amd: 'rxjs/Subject'
-    },
-    'rxjs/Subscription': {
-      root: ['rx', 'Subscription'],
-      commonjs: 'rxjs/Subscription',
-      commonjs2: 'rxjs/Subscription',
-      amd: 'rxjs/Subscription'
-    },
+  externals: [(context, request, callback) => {
+
+    if (request.startsWith('@angular/')) {
+      return callback(null, {
+        root: ['ng', camelCase(request.replace(/^\@angular\//, ''))],
+        commonjs: request,
+        commonjs2: request,
+        amd: request
+      });
+    }
+
+    if (request.startsWith('rxjs/')) {
+      return callback(null, {
+        root: ['rx', request.replace(/^rxjs\//, '')],
+        commonjs: request,
+        commonjs2: request,
+        amd: request
+      });
+    }
+
+    if (request.startsWith('date-fns/')) {
+      return callback(null, {
+        root: ['dateFns', camelCase(request.replace(/^date\-fns\//, ''))],
+        commonjs: `${request}/index`,
+        commonjs2: `${request}/index`,
+        amd: `${request}/index`
+      });
+    }
+
+    callback();
+
+  }, {
     'calendar-utils': {
       root: ['calendarUtils'],
       commonjs: 'calendar-utils',
       commonjs2: 'calendar-utils',
       amd: 'calendar-utils'
     },
-    'date-fns/is_same_day': {
-      root: ['dateFns', 'isSameDay'],
-      commonjs: 'date-fns/is_same_day/index',
-      commonjs2: 'date-fns/is_same_day/index'
-    },
-    'date-fns/get_iso_week': {
-      root: ['dateFns', 'getISOWeek'],
-      commonjs: 'date-fns/get_iso_week/index',
-      commonjs2: 'date-fns/get_iso_week/index'
-    },
-    'date-fns/add_minutes': {
-      root: ['dateFns', 'addMinutes'],
-      commonjs: 'date-fns/add_minutes/index',
-      commonjs2: 'date-fns/add_minutes/index'
-    },
-    'date-fns/add_days': {
-      root: ['dateFns', 'addDays'],
-      commonjs: 'date-fns/add_days/index',
-      commonjs2: 'date-fns/add_days/index'
-    },
-    'date-fns/set_date': {
-      root: ['dateFns', 'setDate'],
-      commonjs: 'date-fns/set_date/index',
-      commonjs2: 'date-fns/set_date/index'
-    },
-    'date-fns/set_month': {
-      root: ['dateFns', 'setMonth'],
-      commonjs: 'date-fns/set_month/index',
-      commonjs2: 'date-fns/set_month/index'
-    },
-    'date-fns/set_year': {
-      root: ['dateFns', 'setYear'],
-      commonjs: 'date-fns/set_year/index',
-      commonjs2: 'date-fns/set_year/index'
-    },
-    'date-fns/get_date': {
-      root: ['dateFns', 'getDate'],
-      commonjs: 'date-fns/get_date/index',
-      commonjs2: 'date-fns/get_date/index'
-    },
-    'date-fns/get_month': {
-      root: ['dateFns', 'getMonth'],
-      commonjs: 'date-fns/get_month/index',
-      commonjs2: 'date-fns/get_month/index'
-    },
-    'date-fns/get_year': {
-      root: ['dateFns', 'getYear'],
-      commonjs: 'date-fns/get_year/index',
-      commonjs2: 'date-fns/get_year/index'
-    },
-    'date-fns/difference_in_seconds': {
-      root: ['dateFns', 'differenceInSeconds'],
-      commonjs: 'date-fns/difference_in_seconds/index',
-      commonjs2: 'date-fns/difference_in_seconds/index'
-    },
-    'date-fns/add_seconds': {
-      root: ['dateFns', 'addSeconds'],
-      commonjs: 'date-fns/add_seconds/index',
-      commonjs2: 'date-fns/add_seconds/index'
-    },
     'angular-resizable-element': {
       root: 'angularResizableElement',
       commonjs: 'angular-resizable-element',
-      commonjs2: 'angular-resizable-element'
+      commonjs2: 'angular-resizable-element',
+      amd: 'angular-resizable-element'
     },
     'angular-draggable-droppable': {
       root: 'angularDraggableDroppable',
       commonjs: 'angular-draggable-droppable',
-      commonjs2: 'angular-draggable-droppable'
+      commonjs2: 'angular-draggable-droppable',
+      amd: 'angular-draggable-droppable'
     }
-  },
+  }],
   module: {
     rules: [{
       enforce: 'pre',
