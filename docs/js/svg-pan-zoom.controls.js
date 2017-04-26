25ddd22a3a5a76b4785305c4ec6a11df4d96d96a
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2017-04-26 17:18:57 +0100

    chore: build demos and docs

--- docs/js/svg-pan-zoom.controls.js
+++ docs/js/svg-pan-zoom.controls.js
@@ -1,22 +1,22 @@
-document.getElementById('demo-svg').addEventListener('load', function() {
-    panZoom = svgPanZoom('#demo-svg', {
+document.addEventListener('DOMContentLoaded', function() {
+    panZoom = svgPanZoom(document.getElementById('module-graph-svg').querySelector('svg'), {
         zoomEnabled: true,
         minZoom: 1,
         maxZoom: 5
     });
 
     document.getElementById('zoom-in').addEventListener('click', function(ev) {
-        ev.preventDefault()
-        panZoom.zoomIn()
+        ev.preventDefault();
+        panZoom.zoomIn();
     });
 
     document.getElementById('zoom-out').addEventListener('click', function(ev) {
-        ev.preventDefault()
-        panZoom.zoomOut()
+        ev.preventDefault();
+        panZoom.zoomOut();
     });
 
     document.getElementById('reset').addEventListener('click', function(ev) {
-        ev.preventDefault()
+        ev.preventDefault();
         panZoom.resetZoom();
         panZoom.resetPan();
     });
