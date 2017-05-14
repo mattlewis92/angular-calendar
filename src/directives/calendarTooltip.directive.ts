4531ebc9cf76bc42e27990d6f3ae21d1ce757ef3
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2017-05-14 19:50:15 +0100

    fix(tooltip): position the tooltip correctly when using AoT

--- src/directives/calendarTooltip.directive.ts
+++ src/directives/calendarTooltip.directive.ts
@@ -2,7 +2,6 @@ import {
   Directive,
   Component,
   HostListener,
-  AfterViewChecked,
   OnDestroy,
   Input,
   ComponentRef,
@@ -127,7 +126,7 @@ export class CalendarTooltipWindowComponent {
 @Directive({
   selector: '[mwlCalendarTooltip]'
 })
-export class CalendarTooltipDirective implements AfterViewChecked, OnDestroy {
+export class CalendarTooltipDirective implements OnDestroy {
 
   @Input('mwlCalendarTooltip') contents: string; // tslint:disable-line no-input-rename
 
@@ -146,13 +145,6 @@ export class CalendarTooltipDirective implements AfterViewChecked, OnDestroy {
     @Inject(DOCUMENT) private document //tslint:disable-line
   ) {
     this.tooltipFactory = componentFactoryResolver.resolveComponentFactory(CalendarTooltipWindowComponent);
-  }
-
-  ngAfterViewChecked(): void {
-    // setTimeout is a fix for https://github.com/mattlewis92/angular-calendar/issues/196
-    setTimeout(() => {
-      this.positionTooltip();
-    });
   }
 
   ngOnDestroy(): void {
@@ -175,6 +167,9 @@ export class CalendarTooltipDirective implements AfterViewChecked, OnDestroy {
       this.tooltipRef.instance.contents = this.contents;
       this.tooltipRef.instance.placement = this.placement;
       this.document.body.appendChild(this.tooltipRef.location.nativeElement);
+      setTimeout(() => {
+        this.positionTooltip();
+      });
     }
   }
 
