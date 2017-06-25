0bb5fc97d6db163a09e92780d6779598d1c770cc
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2017-06-25 21:59:25 +0100

    docs(demos): keep external events demo as minimal as possible

--- demos/demo-modules/draggable-external-events/component.ts
+++ demos/demo-modules/draggable-external-events/component.ts
@@ -1,133 +1,11 @@
-import { Component, ChangeDetectionStrategy, EventEmitter, Output } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import {
   CalendarEvent,
-  CalendarEventTimesChangedEvent,
-  CalendarWeekViewComponent,
-  CalendarUtils,
-  CalendarWeekViewEvent,
-  CalendarGetWeekViewArgs,
-  CalendarWeekViewEventRow
+  CalendarEventTimesChangedEvent
 } from 'angular-calendar';
 import { Subject } from 'rxjs/Subject';
 import { addDays, differenceInDays, startOfDay } from 'date-fns';
 import { colors } from '../demo-utils/colors';
-
-interface MyCalendarEventTimesChangedEvent extends CalendarEventTimesChangedEvent {
-  droppedOutsideCalendar?: boolean;
-}
-
-export class MyCalendarUtils extends CalendarUtils {
-
-  getWeekView(args: CalendarGetWeekViewArgs): CalendarWeekViewEventRow[] {
-    args.absolutePositionedEvents = true;
-    return super.getWeekView(args);
-  }
-
-}
-
-@Component({
-  selector: 'drag-events-outside-week-view-component', // tslint:disable-line
-  template: `
-    <div class="cal-week-view" #weekViewContainer>
-      <mwl-calendar-week-view-header
-        [days]="days"
-        [locale]="locale"
-        [customTemplate]="headerTemplate"
-        (dayClicked)="dayClicked.emit($event)"
-        (eventDropped)="eventTimesChanged.emit($event)">
-      </mwl-calendar-week-view-header>
-      <div class="cal-events">
-        <div
-          *ngFor="let eventRow of eventRows"
-          #eventRowContainer
-          class="cal-events-row"
-          style="height: auto">
-          <div
-            class="cal-event-container"
-            style="position: static"
-            #event
-            [class.cal-draggable]="weekEvent.event.draggable"
-            *ngFor="let weekEvent of eventRow.row"
-            [style.width]="((100 / days.length) * weekEvent.span) + '%'"
-            [style.marginLeft]="((100 / days.length) * weekEvent.offset) + '%'"
-            mwlResizable
-            [resizeEdges]="{left: weekEvent.event?.resizable?.beforeStart, right: weekEvent.event?.resizable?.afterEnd}"
-            [resizeSnapGrid]="{left: getDayColumnWidth(eventRowContainer), right: getDayColumnWidth(eventRowContainer)}"
-            [validateResize]="validateResize"
-            (resizeStart)="resizeStarted(weekViewContainer, weekEvent, $event)"
-            (resizing)="resizing(weekEvent, $event, getDayColumnWidth(eventRowContainer))"
-            (resizeEnd)="resizeEnded(weekEvent)"
-            mwlDraggable
-            [dragAxis]="{
-              x: weekEvent.event.draggable && currentResizes.size === 0,
-              y: weekEvent.event.draggable && currentResizes.size === 0
-            }"
-            [dropData]="{event: weekEvent.event}"
-            (dragEnd)="eventDragged(weekEvent, $event.x, getDayColumnWidth(eventRowContainer))">
-            <mwl-calendar-week-view-event
-              [weekEvent]="weekEvent"
-              [tooltipPlacement]="tooltipPlacement"
-              [tooltipTemplate]="tooltipTemplate"
-              [customTemplate]="eventTemplate"
-              (eventClicked)="eventClicked.emit({event: weekEvent.event})">
-            </mwl-calendar-week-view-event>
-          </div>
-        </div>
-      </div>
-    </div>
-  `,
-  providers: [{
-    provide: CalendarUtils,
-    useClass: MyCalendarUtils
-  }]
-})
-export class DragEventsOutsideWeekViewComponent extends CalendarWeekViewComponent {
-
-  @Output() eventTimesChanged: EventEmitter<MyCalendarEventTimesChangedEvent> = new EventEmitter();
-
-  eventDragged(weekEvent: CalendarWeekViewEvent, draggedByPx: number, dayWidth: number): void {
-
-    let daysDragged: number = Math.round(draggedByPx / dayWidth);
-    let newStart: Date = addDays(weekEvent.event.start, daysDragged);
-    let droppedOutsideCalendar: boolean = false;
-
-    // Restrict start to first and last day on current week
-    if (newStart < this.days[0].date) {
-      daysDragged += differenceInDays(startOfDay(this.days[0].date), startOfDay(newStart));
-      droppedOutsideCalendar = true;
-    }
-    const lastDate: Date = this.days[this.days.length - 1].date;
-    if (newStart > lastDate) {
-      daysDragged -= differenceInDays(startOfDay(newStart), startOfDay(lastDate));
-      droppedOutsideCalendar = true;
-    }
-
-    newStart = addDays(weekEvent.event.start, daysDragged);
-    let newEnd: Date;
-    if (weekEvent.event.end) {
-      newEnd = addDays(weekEvent.event.end, daysDragged);
-    }
-
-    this.eventTimesChanged.emit({newStart, newEnd, event: weekEvent.event, droppedOutsideCalendar});
-
-  }
-
-  dragStart(weekViewContainer: HTMLElement, event: HTMLElement): void {
-
-    event.parentNode.insertBefore(event.cloneNode(), event.nextSibling);
-
-    // With a scrollbar during the drag, the event is only visible inside the calendar.
-    // The "fixed" position bring the event on top, even when dragged outside the calendar.
-    const eventRect: ClientRect = event.getBoundingClientRect();
-    event.style.left = eventRect.left + 'px';
-    event.style.top = eventRect.top + 'px';
-    event.style.width = eventRect.width + 'px';
-    event.style.position = 'fixed';
-    event.style.marginLeft = '';
-
-  }
-
-}
 
 @Component({
   selector: 'mwl-demo-component',
@@ -158,34 +36,20 @@ export class DemoComponent {
 
   refresh: Subject<any> = new Subject();
 
-  eventDropped({event, newStart, newEnd, droppedOutsideCalendar}: MyCalendarEventTimesChangedEvent): void {
+  eventDropped({event, newStart, newEnd}: CalendarEventTimesChangedEvent): void {
 
-    if (!droppedOutsideCalendar) {
-      const externalIndex: number = this.externalEvents.indexOf(event);
-      if (externalIndex > -1) {
-        this.externalEvents.splice(externalIndex, 1);
-        this.events.push(event);
-      }
-      event.start = newStart;
-      if (newEnd) {
-        event.end = newEnd;
-      }
-      this.viewDate = newStart;
-      this.activeDayIsOpen = true;
+    const externalIndex: number = this.externalEvents.indexOf(event);
+    if (externalIndex > -1) {
+      this.externalEvents.splice(externalIndex, 1);
+      this.events.push(event);
     }
-
-  }
-
-  droppedBack(event: CalendarEvent): void {
-
-    const internalIndex: number = this.events.indexOf(event);
-
-    if (internalIndex > -1) {
-      this.events.splice(internalIndex, 1);
-      this.externalEvents.push(event);
-
-      this.refresh.next();
+    event.start = newStart;
+    if (newEnd) {
+      event.end = newEnd;
     }
+    this.viewDate = newStart;
+    this.activeDayIsOpen = true;
+
   }
 
 }
--- demos/demo-modules/draggable-external-events/module.ts
+++ demos/demo-modules/draggable-external-events/module.ts
@@ -2,21 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalendarModule } from 'angular-calendar';
 import { DragAndDropModule } from 'angular-draggable-droppable';
-import { ResizableModule } from 'angular-resizable-element';
 import { DemoUtilsModule } from '../demo-utils/module';
-import { DemoComponent, DragEventsOutsideWeekViewComponent } from './component';
+import { DemoComponent } from './component';
 
 @NgModule({
   imports: [
     CommonModule,
     CalendarModule.forRoot(),
     DragAndDropModule,
-    ResizableModule,
     DemoUtilsModule
   ],
   declarations: [
-    DemoComponent,
-    DragEventsOutsideWeekViewComponent
+    DemoComponent
   ],
   exports: [
     DemoComponent
