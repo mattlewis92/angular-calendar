d6c89bc8e82fdc1ef5c0461854a29ea6e4e1bd7c
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2017-07-23 16:25:41 +0100

    docs(demos): remove -p flag from webpack cli

--- webpack.config.ts
+++ webpack.config.ts
@@ -6,107 +6,112 @@ import * as HtmlWebpackPlugin from 'html-webpack-plugin';
 import { getIfUtils, removeEmpty } from 'webpack-config-utils';
 import { AotPlugin } from '@ngtools/webpack';
 
-const env = process.argv.indexOf('-p') > -1 ? 'production' : 'development';
-const { ifProduction, ifDevelopment } = getIfUtils(env);
+export default (env = 'development') => {
 
-export default {
-  devtool: ifProduction('source-map', 'eval'),
-  entry: path.join(__dirname, 'demos', 'entry.ts'),
-  output: {
-    filename: ifProduction('[name]-[chunkhash].js', '[name].js')
-  },
-  module: {
-    rules: removeEmpty([ifDevelopment({
-      enforce: 'pre',
-      test: /\.ts$/,
-      loader: 'prettier-loader',
-      exclude: /node_modules/,
-      options: {
-        singleQuote: true,
-        parser: 'typescript'
-      }
-    }), ifDevelopment({
-      enforce: 'pre',
-      test: /\.ts$/,
-      loader: 'tslint-loader',
-      exclude: /node_modules/
-    }), ifDevelopment({
-      test: /\.ts$/,
-      use: [{
-        loader: 'awesome-typescript-loader',
+  const { ifProduction, ifDevelopment } = getIfUtils(env);
+
+  return {
+    devtool: ifProduction('source-map', 'eval'),
+    entry: path.join(__dirname, 'demos', 'entry.ts'),
+    output: {
+      filename: ifProduction('[name]-[chunkhash].js', '[name].js')
+    },
+    module: {
+      rules: removeEmpty([ifDevelopment({
+        enforce: 'pre',
+        test: /\.ts$/,
+        loader: 'prettier-loader',
+        exclude: /node_modules/,
         options: {
-          module: 'esnext'
+          singleQuote: true,
+          parser: 'typescript'
+        }
+      }), ifDevelopment({
+        enforce: 'pre',
+        test: /\.ts$/,
+        loader: 'tslint-loader',
+        exclude: /node_modules/
+      }), ifDevelopment({
+        test: /\.ts$/,
+        use: [{
+          loader: 'awesome-typescript-loader',
+          options: {
+            module: 'esnext'
+          }
+        }, {
+          loader: 'angular2-template-loader'
+        }, {
+          loader: 'angular-router-loader'
+        }],
+        exclude: /node_modules/
+      }, {
+        test: /\.ts$/,
+        loader: '@ngtools/webpack'
+      }), {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader'
+      }, {
+        test: /(node_modules).+\.css$/,
+        loader: 'style-loader!css-loader'
+      }, {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          mimetype: 'application/font-woff'
         }
       }, {
-        loader: 'angular2-template-loader'
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader'
       }, {
-        loader: 'angular-router-loader'
-      }],
-      exclude: /node_modules/
-    }, {
-      test: /\.ts$/,
-      loader: '@ngtools/webpack'
-    }), {
-      test: /\.scss$/,
-      loader: 'style-loader!css-loader!sass-loader'
-    }, {
-      test: /(node_modules).+\.css$/,
-      loader: 'style-loader!css-loader'
-    }, {
-      test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      loader: 'url-loader',
-      options: {
-        limit: 10000,
-        mimetype: 'application/font-woff'
+        test: /demos[\/\\].+\.(css|html)$/,
+        loader: 'raw-loader'
+      }, {
+        test: /\.ejs$/,
+        loader: 'ejs-compiled-loader'
+      }])
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+      alias: {
+        'angular-calendar$': path.resolve(__dirname, 'src/index.ts')
       }
-    }, {
-      test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      loader: 'file-loader'
-    }, {
-      test: /demos[\/\\].+\.(css|html)$/,
-      loader: 'raw-loader'
-    }, {
-      test: /\.ejs$/,
-      loader: 'ejs-compiled-loader'
-    }])
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-    alias: {
-      'angular-calendar$': path.resolve(__dirname, 'src/index.ts')
-    }
-  },
-  devServer: {
-    port: 8000,
-    inline: true,
-    hot: true,
-    historyApiFallback: true
-  },
-  plugins: removeEmpty([
-    ifDevelopment(new CheckerPlugin()),
-    ifDevelopment(new TsConfigPathsPlugin()),
-    ifDevelopment(new webpack.HotModuleReplacementPlugin()),
-    ifProduction(new AotPlugin({
-      tsConfigPath: './tsconfig-demos.json'
-    })),
-    new webpack.DefinePlugin({
-      ENV: JSON.stringify(env)
-    }),
-    ifDevelopment(new StyleLintPlugin({
-      syntax: 'scss',
-      context: 'scss'
-    })),
-    new webpack.ContextReplacementPlugin(
-      /angular(\\|\/)core(\\|\/)@angular/,
-      __dirname + '/src'
-    ),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'main',
-      async: true,
-      minChunks: 2
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'demos', 'index.ejs')
-    })
-  ])
+    },
+    devServer: {
+      port: 8000,
+      inline: true,
+      hot: true,
+      historyApiFallback: true
+    },
+    plugins: removeEmpty([
+      ifDevelopment(new CheckerPlugin()),
+      ifDevelopment(new TsConfigPathsPlugin()),
+      ifDevelopment(new webpack.HotModuleReplacementPlugin()),
+      ifProduction(new AotPlugin({
+        tsConfigPath: './tsconfig-demos.json'
+      })),
+      new webpack.DefinePlugin({
+        ENV: JSON.stringify(env)
+      }),
+      ifProduction(new webpack.optimize.UglifyJsPlugin({
+        sourceMap: true
+      })),
+      ifDevelopment(new StyleLintPlugin({
+        syntax: 'scss',
+        context: 'scss'
+      })),
+      new webpack.ContextReplacementPlugin(
+        /angular(\\|\/)core(\\|\/)@angular/,
+        __dirname + '/src'
+      ),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'main',
+        async: true,
+        minChunks: 2
+      }),
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'demos', 'index.ejs')
+      })
+    ])
+  }
 };
