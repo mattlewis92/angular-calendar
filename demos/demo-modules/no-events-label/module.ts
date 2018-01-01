a3ed9bca30ed8ecbfe88ad4e5cfd1ea4fba56a43
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2018-01-01 18:33:49 +0000

    docs(demos): add the no-events-label demo
    Closes #411

---
+++ demos/demo-modules/no-events-label/module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { CalendarModule } from 'angular-calendar';
+import { DemoUtilsModule } from '../demo-utils/module';
+import { DemoComponent } from './component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    CalendarModule.forRoot(),
+    DemoUtilsModule,
+    RouterModule.forChild([{ path: '', component: DemoComponent }])
+  ],
+  declarations: [DemoComponent],
+  exports: [DemoComponent]
+})
+export class DemoModule {}
