15a73cc8f8949a9751383a839cd62821af22d36e
Daan Knippers <daanknip@outlook.com>
2018-05-31 10:30:42 +0200

    fix(event): ignore click event propagation

--- src/modules/day/calendar-day-view-event.component.ts
+++ src/modules/day/calendar-day-view-event.component.ts
@@ -26,7 +26,7 @@ import { DayViewEvent } from 'calendar-utils';
         [tooltipEvent]="dayEvent.event"
         [tooltipTemplate]="tooltipTemplate"
         [tooltipAppendToBody]="tooltipAppendToBody"
-        (mwlClick)="eventClicked.emit()">
+        (mwlClick)="handleEventClick($event)">
         <mwl-calendar-event-actions [event]="dayEvent.event"></mwl-calendar-event-actions>
         &ngsp;
         <mwl-calendar-event-title
@@ -62,4 +62,17 @@ export class CalendarDayViewEventComponent {
   @Input() tooltipTemplate: TemplateRef<any>;
 
   @Output() eventClicked: EventEmitter<any> = new EventEmitter();
+
+  /**
+   * @hidden
+   */
+  handleEventClick(clickEvent: any) {
+    // only fire click event when clicked on host element or title therein, ignore other underlying elements
+    if (
+      clickEvent.target.classList.contains('cal-event') ||
+      clickEvent.target.classList.contains('cal-event-title')
+    ) {
+      this.eventClicked.emit();
+    }
+  }
 }
