01b0c14b8262349f8b7a8415008799241b9520ec
Matt Lewis <hello@mattlewis.me>
2018-06-22 22:53:07 +0100

    build: use star imports for date-fns

--- src/date-adapters/date-fns.ts
+++ src/date-adapters/date-fns.ts
@@ -1,16 +1,16 @@
 import { adapterFactory as baseAdapterFactory } from 'calendar-utils/date-adapters/date-fns';
-import addWeeks from 'date-fns/add_weeks/index';
-import addMonths from 'date-fns/add_months/index';
-import subDays from 'date-fns/sub_days/index';
-import subWeeks from 'date-fns/sub_weeks/index';
-import subMonths from 'date-fns/sub_months/index';
-import getISOWeek from 'date-fns/get_iso_week/index';
-import setDate from 'date-fns/set_date/index';
-import setMonth from 'date-fns/set_month/index';
-import setYear from 'date-fns/set_year/index';
-import getDate from 'date-fns/get_date/index';
-import getMonth from 'date-fns/get_month/index';
-import getYear from 'date-fns/get_year/index';
+import * as addWeeks from 'date-fns/add_weeks/index';
+import * as addMonths from 'date-fns/add_months/index';
+import * as subDays from 'date-fns/sub_days/index';
+import * as subWeeks from 'date-fns/sub_weeks/index';
+import * as subMonths from 'date-fns/sub_months/index';
+import * as getISOWeek from 'date-fns/get_iso_week/index';
+import * as setDate from 'date-fns/set_date/index';
+import * as setMonth from 'date-fns/set_month/index';
+import * as setYear from 'date-fns/set_year/index';
+import * as getDate from 'date-fns/get_date/index';
+import * as getMonth from 'date-fns/get_month/index';
+import * as getYear from 'date-fns/get_year/index';
 import { DateAdapter } from './date-adapter';
 
 export function adapterFactory(): DateAdapter {
