db0c8806d010970ac77eae34544130c191c0ff13
Matt Lewis <hello@mattlewis.me>
2018-07-02 16:16:02 +0100

    feat(month-view): allow the open day events animation to be overridden
    BREAKING CHANGE:
    If using a custom `openDayEventsTemplate` for the month view you must now wrap your template with:
    ```
    <div class="cal-open-day-events" [@collapse] *ngIf="isOpen"></div>
    ```
    
    and then you must add the collapse animation to the component that contains the open day events `<ng-template>`:
    
    ```
    import { collapseAnimation } from 'angular-calendar';
    
    // add this to your component metadata
    animations: [collapseAnimation]
    ```

--- src/modules/month/calendar-month.module.ts
+++ src/modules/month/calendar-month.module.ts
@@ -13,6 +13,7 @@ export {
   CalendarMonthViewEventTimesChangedEvent
 } from './calendar-month-view.component';
 export { MonthViewDay as CalendarMonthViewDay } from 'calendar-utils';
+export { collapseAnimation } from './calendar-open-day-events.component';
 
 @NgModule({
   imports: [CommonModule, DragAndDropModule, CalendarCommonModule],
--- src/modules/month/calendar-open-day-events.component.ts
+++ src/modules/month/calendar-open-day-events.component.ts
@@ -5,9 +5,26 @@ import {
   EventEmitter,
   TemplateRef
 } from '@angular/core';
-import { trigger, style, transition, animate } from '@angular/animations';
+import {
+  trigger,
+  style,
+  transition,
+  animate,
+  AnimationTriggerMetadata
+} from '@angular/animations';
 import { CalendarEvent } from 'calendar-utils';
 import { trackByEventId } from '../common/util';
+
+export const collapseAnimation: AnimationTriggerMetadata = trigger('collapse', [
+  transition('void => *', [
+    style({ height: 0, overflow: 'hidden' }),
+    animate('150ms', style({ height: '*' }))
+  ]),
+  transition('* => void', [
+    style({ height: '*', overflow: 'hidden' }),
+    animate('150ms', style({ height: 0 }))
+  ])
+]);
 
 @Component({
   selector: 'mwl-calendar-open-day-events',
@@ -15,52 +32,43 @@ import { trackByEventId } from '../common/util';
     <ng-template
       #defaultTemplate
       let-events="events"
-      let-eventClicked="eventClicked">
-      <div
-        *ngFor="let event of events; trackBy:trackByEventId"
-        [ngClass]="event?.cssClass"
-        mwlDraggable
-        [class.cal-draggable]="event.draggable"
-        dragActiveClass="cal-drag-active"
-        [dropData]="{event: event}"
-        [dragAxis]="{x: event.draggable, y: event.draggable}">
-        <span
-          class="cal-event"
-          [style.backgroundColor]="event.color?.primary">
-        </span>
-        &ngsp;
-        <mwl-calendar-event-title
-          [event]="event"
-          [customTemplate]="eventTitleTemplate"
-          view="month"
-          (mwlClick)="eventClicked.emit({event: event})">
-        </mwl-calendar-event-title>
-        &ngsp;
-        <mwl-calendar-event-actions [event]="event"></mwl-calendar-event-actions>
+      let-eventClicked="eventClicked"
+      let-isOpen="isOpen">
+      <div class="cal-open-day-events" [@collapse] *ngIf="isOpen">
+        <div
+          *ngFor="let event of events; trackBy:trackByEventId"
+          [ngClass]="event?.cssClass"
+          mwlDraggable
+          [class.cal-draggable]="event.draggable"
+          dragActiveClass="cal-drag-active"
+          [dropData]="{event: event}"
+          [dragAxis]="{x: event.draggable, y: event.draggable}">
+          <span
+            class="cal-event"
+            [style.backgroundColor]="event.color?.primary">
+          </span>
+          &ngsp;
+          <mwl-calendar-event-title
+            [event]="event"
+            [customTemplate]="eventTitleTemplate"
+            view="month"
+            (mwlClick)="eventClicked.emit({event: event})">
+          </mwl-calendar-event-title>
+          &ngsp;
+          <mwl-calendar-event-actions [event]="event"></mwl-calendar-event-actions>
+        </div>
       </div>
     </ng-template>
-    <div class="cal-open-day-events" [@collapse] *ngIf="isOpen">
-      <ng-template
-        [ngTemplateOutlet]="customTemplate || defaultTemplate"
-        [ngTemplateOutletContext]="{
-          events: events,
-          eventClicked: eventClicked
-        }">
-      </ng-template>
-    </div>
+    <ng-template
+      [ngTemplateOutlet]="customTemplate || defaultTemplate"
+      [ngTemplateOutletContext]="{
+        events: events,
+        eventClicked: eventClicked,
+        isOpen: isOpen
+      }">
+    </ng-template>
   `,
-  animations: [
-    trigger('collapse', [
-      transition('void => *', [
-        style({ height: 0, overflow: 'hidden' }),
-        animate('150ms', style({ height: '*' }))
-      ]),
-      transition('* => void', [
-        style({ height: '*', overflow: 'hidden' }),
-        animate('150ms', style({ height: 0 }))
-      ])
-    ])
-  ]
+  animations: [collapseAnimation]
 })
 export class CalendarOpenDayEventsComponent {
   @Input() isOpen: boolean = false;
