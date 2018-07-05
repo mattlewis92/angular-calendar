daa2e00b1510c59d5d51ab82f592a2d5729ae0bf
Matt Lewis <hello@mattlewis.me>
2018-07-05 10:03:31 +0100

    docs(demos): swap demo utils date time picker for one thats on npm
    Fixes #605
    Fixes #624

--- demos/demo-modules/kitchen-sink/module.ts
+++ demos/demo-modules/kitchen-sink/module.ts
@@ -1,9 +1,10 @@
+import 'flatpickr/dist/flatpickr.css';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CalendarModule } from 'angular-calendar';
 import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
-import { DemoUtilsModule } from '../demo-utils/module';
+import { FlatpickrModule } from 'angularx-flatpickr';
 import { DemoComponent } from './component';
 
 @NgModule({
@@ -11,8 +12,8 @@ import { DemoComponent } from './component';
     CommonModule,
     FormsModule,
     NgbModalModule.forRoot(),
-    CalendarModule.forRoot(),
-    DemoUtilsModule
+    FlatpickrModule.forRoot(),
+    CalendarModule.forRoot()
   ],
   declarations: [DemoComponent],
   exports: [DemoComponent]
