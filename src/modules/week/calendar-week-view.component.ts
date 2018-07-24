56169128f193b95beac8850dcc17d9bb131f10f8
Matt Lewis <hello@mattlewis.me>
2018-07-24 09:49:33 -0700

    fix: disable drag when dropping events outside of the view

--- src/modules/week/calendar-week-view.component.ts
+++ src/modules/week/calendar-week-view.component.ts
@@ -734,6 +734,7 @@ export class CalendarWeekViewComponent implements OnChanges, OnInit, OnDestroy {
         (dropEvent.dropData.event.allDay && !allDay) ||
         (!dropEvent.dropData.event.allDay && allDay))
     ) {
+      this.dragActive = false;
       this.eventTimesChanged.emit({
         type: CalendarEventTimesChangedEventType.Drop,
         event: dropEvent.dropData.event,
