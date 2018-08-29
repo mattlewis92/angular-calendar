6bb89fe49eaa7500ee2758aa7191bb53aa0cc75c
Matt Lewis <hello@mattlewis.me>
2018-08-29 08:31:44 -0700

    build: upgrade dependencies

--- demos/demo-modules/kitchen-sink/module.ts
+++ demos/demo-modules/kitchen-sink/module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { FlatpickrModule } from 'angularx-flatpickr';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
-import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap/modal/modal.module';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 import { DemoComponent } from './component';
 
 @NgModule({
