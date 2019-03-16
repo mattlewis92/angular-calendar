ebbe3a40d046ad8c1ba852acead9ea731893fd36
Phillip Marsden <philyorkshire@philyorkshire.uk>
2019-03-16 05:03:41 +0000

    docs: fix async events demo in safari
    Fixes #883

--- projects/demos/app/demo-modules/async-events/component.ts
+++ projects/demos/app/demo-modules/async-events/component.ts
@@ -29,7 +29,8 @@ function getTimezoneOffsetString(date: Date): string {
   ).padStart(2, '0');
   const minutesOffset = String(Math.abs(timezoneOffset % 60)).padEnd(2, '0');
   const direction = timezoneOffset > 0 ? '-' : '+';
-  return `T00:00:00${direction}${hoursOffset}${minutesOffset}`;
+
+  return `T00:00:00${direction}${hoursOffset}:${minutesOffset}`;
 }
 
 @Component({
