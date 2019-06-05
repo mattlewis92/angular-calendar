934ba918296224377bdb02e95a4929838e93d6e8
Matt Lewis <hello@mattlewis.me>
2019-06-05 15:08:21 -0400

    Auto-generated commit

---
+++ common-es2015.30833cd48ab16d926220.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{G6HM:function(n,r,e){"use strict";e.d(r,"a",function(){return o});const o={red:{primary:"#ad2121",secondary:"#FAE3E3"},blue:{primary:"#1e90ff",secondary:"#D1E8FF"},yellow:{primary:"#e3bc08",secondary:"#FDF1BA"}}}}]);
