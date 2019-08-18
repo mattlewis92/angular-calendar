c78dea5bdd0e29d8d576da8ac3eee8905effe665
Matt Lewis <hello@mattlewis.me>
2019-08-18 14:50:31 +0100

    docs(week-view): add note about using weekStartsOn in the title
    Closes #1048

--- projects/angular-calendar/src/modules/week/calendar-week-view.component.ts
+++ projects/angular-calendar/src/modules/week/calendar-week-view.component.ts
@@ -404,7 +404,9 @@ export class CalendarWeekViewComponent implements OnChanges, OnInit, OnDestroy {
   @Input() tooltipDelay: number | null = null;
 
   /**
-   * The start number of the week. This is ignored when the `daysInWeek` input is also set as the `viewDate` will be used as the start of the week instead
+   * The start number of the week.
+   * This is ignored when the `daysInWeek` input is also set as the `viewDate` will be used as the start of the week instead.
+   * Note, you should also pass this to the calendar title pipe so it shows the same days: {{ viewDate | calendarDate:(view + 'ViewTitle'):locale:weekStartsOn }}
    */
   @Input() weekStartsOn: number;
 
