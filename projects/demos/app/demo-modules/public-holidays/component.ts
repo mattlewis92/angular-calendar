091ef1ddaaed9b06f60ff5d2f922cbb7aa108603
Matt Lewis <hello@mattlewis.me>
2020-05-03 12:28:07 +0100

    docs(demos): add holiday meta

--- projects/demos/app/demo-modules/public-holidays/component.ts
+++ projects/demos/app/demo-modules/public-holidays/component.ts
@@ -14,6 +14,15 @@ const HOLIDAY_API_KEY = '8eb2582d-3a4c-4fc5-94c8-3e21487d4e23';
 // change this to your own country
 const COUNTRY_CODE = 'US';
 
+interface Holiday {
+  date: string;
+  name: string;
+}
+
+type CalendarEventWithMeta = CalendarEvent<
+  { type: 'holiday'; holiday: Holiday } | { type: 'normal' }
+>;
+
 @Component({
   selector: 'mwl-demo-component',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -24,7 +33,7 @@ export class DemoComponent implements OnInit {
 
   viewDate = startOfYear(subYears(new Date(), 1));
 
-  events: CalendarEvent[] = [];
+  events: CalendarEventWithMeta[] = [];
 
   constructor(private http: HttpClient, private cdr: ChangeDetectorRef) {}
 
@@ -34,22 +43,23 @@ export class DemoComponent implements OnInit {
 
   private fetchHolidays() {
     this.http
-      .get<{ holidays: { date: string; name: string }[] }>(
-        'https://holidayapi.com/v1/holidays',
-        {
-          params: {
-            country: COUNTRY_CODE,
-            year: String(new Date().getFullYear() - 1),
-            key: HOLIDAY_API_KEY,
-          },
-        }
-      )
+      .get<{ holidays: Holiday[] }>('https://holidayapi.com/v1/holidays', {
+        params: {
+          country: COUNTRY_CODE,
+          year: String(new Date().getFullYear() - 1),
+          key: HOLIDAY_API_KEY,
+        },
+      })
       .subscribe(({ holidays }) => {
         this.events = holidays.map((holiday) => {
           return {
             start: new Date(holiday.date),
             title: holiday.name,
             allDay: true,
+            meta: {
+              type: 'holiday',
+              holiday,
+            },
           };
         });
         this.cdr.markForCheck();
