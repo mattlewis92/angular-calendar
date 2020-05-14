4afe3ae50eecfc04461080064b5946e5e9c0be5d
Matt Lewis <hello@mattlewis.me>
2020-05-14 10:51:23 +0100

    fix(schematics): fix error on install
    Fixes #1253

--- projects/angular-calendar/schematics/ng-add/version-names.ts
+++ projects/angular-calendar/schematics/ng-add/version-names.ts
@@ -1,4 +1,4 @@
-const packageJson = require('../../package.json'); // tslint:disable-line no-var-requires
+const packageJson = require('./package.json'); // tslint:disable-line no-var-requires
 
 export const angularCalendarVersion = `^${packageJson.version}`;
 export const momentVersion = packageJson.devDependencies.moment;
