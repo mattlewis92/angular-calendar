641c13c1fab855d4cb80cf691e96a7f91d430b00
Matt Lewis <hello@mattlewis.me>
2020-09-22 15:18:45 +0200

    docs(demos): fix timezone offset in custom date formatters
    Fixes #1341

--- projects/demos/app/demo-modules/customise-date-formats/custom-date-formatter.provider.ts
+++ projects/demos/app/demo-modules/customise-date-formats/custom-date-formatter.provider.ts
@@ -1,5 +1,5 @@
 import { CalendarDateFormatter, DateFormatterParams } from 'angular-calendar';
-import { DatePipe } from '@angular/common';
+import { formatDate } from '@angular/common';
 import { Injectable } from '@angular/core';
 
 @Injectable()
@@ -7,18 +7,18 @@ export class CustomDateFormatter extends CalendarDateFormatter {
   // you can override any of the methods defined in the parent class
 
   public monthViewColumnHeader({ date, locale }: DateFormatterParams): string {
-    return new DatePipe(locale).transform(date, 'EEE', locale);
+    return formatDate(date, 'EEE', locale);
   }
 
   public monthViewTitle({ date, locale }: DateFormatterParams): string {
-    return new DatePipe(locale).transform(date, 'MMM y', locale);
+    return formatDate(date, 'MMM y', locale);
   }
 
   public weekViewColumnHeader({ date, locale }: DateFormatterParams): string {
-    return new DatePipe(locale).transform(date, 'EEE', locale);
+    return formatDate(date, 'EEE', locale);
   }
 
   public dayViewHour({ date, locale }: DateFormatterParams): string {
-    return new DatePipe(locale).transform(date, 'HH:mm', locale);
+    return formatDate(date, 'HH:mm', locale);
   }
 }
