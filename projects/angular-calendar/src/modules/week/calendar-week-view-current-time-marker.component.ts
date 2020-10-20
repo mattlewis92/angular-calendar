7eef53a6737d05445969f8569747f73e734724dd
Matt Lewis <hello@mattlewis.me>
2020-10-20 19:32:13 +0100

    build: upgrade to angular 10

--- projects/angular-calendar/src/modules/week/calendar-week-view-current-time-marker.component.ts
+++ projects/angular-calendar/src/modules/week/calendar-week-view-current-time-marker.component.ts
@@ -61,7 +61,7 @@ export class CalendarWeekViewCurrentTimeMarkerComponent implements OnChanges {
 
   @Input() customTemplate: TemplateRef<any>;
 
-  columnDate$ = new BehaviorSubject<Date>(this.columnDate);
+  columnDate$ = new BehaviorSubject<Date>(null);
 
   marker$: Observable<{
     isVisible: boolean;
