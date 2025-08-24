77c8c5caf66ebcf189191ecf4bd3c5b2eba55cbb
Matt Lewis <matt@clickup.com>
2025-08-24 20:20:58 +0100

    test: fix failing tests

--- projects/angular-calendar/karma.conf.ts
+++ projects/angular-calendar/karma.conf.ts
@@ -10,6 +10,7 @@ module.exports = (config) => {
       require('karma-mocha-reporter'),
       require('karma-chrome-launcher'),
       require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma'),
     ],
     coverageReporter: {
       dir: require('path').join(__dirname, '../../coverage'),
